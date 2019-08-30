b043df2da7b04e7ae825e72b90a5c2404bfbfb1d
LeviSylvester <54448101+LeviSylvester@users.noreply.github.com>
2019-08-30 06:13:39 +0300

    Add files via upload

---
+++ W2StringsASCIICompSum.java
@@ -0,0 +1,49 @@
+import java.io.StringReader;
+
+/*
+2. Declarati si initializati 2 variabile de tip String.
+Afisati care dintre acestea are suma valorilor caracterelor in codul ASCII mai mare
+(ex: AAB mai mic decat zz -> "AAB" = 65+65+66 = 196, "zz" = 122+122 = 244  -> 196 < 244).
+ */
+public class W2StringsASCIICompSum {
+    public static void main(String[] args) {
+        //this doesn't to exactly what's asked:
+//        String a = "A1";
+//        String b = "A1";
+//        if (a.compareTo(b)>0) {
+//            System.out.println(a+" mai mare decat "+b);
+//        } else if (a.compareTo(b)<0) {
+//            System.out.println(b+" mai mare decat "+a);
+//        } else {
+//            System.out.println("sum of their characters' value are equal");
+//        }
+
+        //this does exactly what is asked:
+        String firstString = "ABC";
+        String secondString = "DEF";
+
+        int length1 = firstString.length();
+        int charATi1;
+        int charSum1 = 0;
+        for (int i = 0; i < length1; i++) {
+            charATi1 = firstString.charAt(i);
+            charSum1 += charATi1;
+        }
+
+        int length2 = secondString.length();
+        int charATi2;
+        int charSum2 = 0;
+        for (int j = 0; j < length2; j++) {
+            charATi2 = secondString.charAt(j);
+            charSum2 += charATi2;
+        }
+
+        if (charSum1 > charSum2) {
+            System.out.println(firstString + " mai mare decat " + secondString);
+        } else if (charSum2 > charSum1) {
+            System.out.println(secondString + " mai mare decat " + firstString);
+        } else {
+            System.out.println("sum of their characters' value are equal");
+        }
+    }
+}
